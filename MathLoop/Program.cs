0ec9120665f5ba4f3a77886d976854c540cd9aa4
sethradmall <sethradmall@gmail.com>
2018-08-17 22:39:48 -0600

    Add project files.

---
+++ MathLoop/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MathLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program math = new Program();
+            int output = math.Number();
+            int number1 = output;
+            for (int i = 1; i <= number1; i++)
+            {
+                output = i * output;
+            }
+            Console.WriteLine(output);
+        }
+        int Number()
+        {
+            int number = 0;
+            bool dontrepeat = false;
+            Console.WriteLine("Enter Number");
+            do
+            {
+                string input = (Console.ReadLine());
+                if (int.TryParse(input, out number))
+                {
+                    dontrepeat = true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Entry");
+                    Console.WriteLine("Enter Number");
+                }
+            } while (dontrepeat == false);
+            return number;
+        }
+    }
+}
